c129a4e9aab0c479a3dfcbf19ec71fe25fda7ef5
Matheus <matheus.de.souza.code@gmail.com>
2025-09-11 21:45:29 -0300

    backend antigo removido

--- src/main/java/com/kitsparana/backend/infra/BusinessException.java
+++
@@ -1,5 +0,0 @@
-package com.kitsparana.backend.infra;
-
-public class BusinessException extends RuntimeException {
-    public BusinessException(String msg) {super(msg);}
-}
--- src/main/java/com/kitsparana/backend/infra/NotFoundException.java
+++
@@ -1,5 +0,0 @@
-package com.kitsparana.backend.infra;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String msg) { super(msg); }
-}
